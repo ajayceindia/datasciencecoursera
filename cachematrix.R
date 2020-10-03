9bd8e6050cb06c6239e8e099259a18546fbf25ea
ajayceindia <ajayceindia@yahoo.com>
2020-10-04 05:28:32 +0530

    Assignment Submission

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## Document for an Assignment
+## functions do
+
+## To create a special Matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+## To compute the inverse of the special matrix
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setinverse(m)
+    m
+}
+  
+  
